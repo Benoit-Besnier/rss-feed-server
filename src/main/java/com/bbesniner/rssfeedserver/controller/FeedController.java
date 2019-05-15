f28ae5de83a718dfcebc3d2c32b59c80f9422221
Benoit Besnier <besnier.benoit0@gmail.com>
2019-05-15 22:35:18 +0200

    fix: add CORS compatibility

--- src/main/java/com/bbesniner/rssfeedserver/controller/FeedController.java
+++ src/main/java/com/bbesniner/rssfeedserver/controller/FeedController.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import java.net.URI;
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping(FeedController.PATH)
 @RequiredArgsConstructor
